89ca0b54e72627bedcf91c7054a5be5bb549363c
hector <hectornuno1994@gmail.com>
2022-03-30 16:55:07 -0700

    adds index route, index action, and encrypts access key for api

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -1,5 +1,6 @@
 class NewsArticlesController < ApplicationController
   def index
-    request = HTTP.get("http://api.mediastack.com/v1/")
+    request = HTTP.get("http://api.mediastack.com/v1/news?access_key=#{Rails.application.credentials.news_api.access_key}&languages=en&categories=technology")
+    render json: request.parse(:json)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "news_articles" => "news_articles#index"
+  get "/news_articles" => "news_articles#index"
 end
 
 # 470e0e46553c3210a1c06c14ad4fd091
